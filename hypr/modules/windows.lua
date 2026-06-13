798f7ceb49a59f5e09cb92bf8f400e648c65f74f
rdogz <213889694+rdogz@users.noreply.github.com>
2026-06-12 21:43:03 -0300

    Remove blur from hypr

--- hypr/modules/windows.lua
+++ hypr/modules/windows.lua
@@ -35,7 +35,7 @@ hl.config({
         },
 
         blur = {
-            enabled   = true,
+            enabled   = false,
             size      = 3,
             passes    = 1,
             vibrancy  = 0.1696,
