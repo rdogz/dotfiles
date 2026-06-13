d548edf003f121db88c756eec575e5ce9a9eee07
rdogz <213889694+rdogz@users.noreply.github.com>
2026-06-12 21:34:47 -0300

    Remove unnecessary scripts

--- scripts/dark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.desktop.interface gtk-theme Materia-dark-compact
-gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-hyprctl setcursor Bibata-Modern-Ice 20
